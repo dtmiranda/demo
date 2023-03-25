ca4dbfe159f0f873968f12e3f985dfd1a7ae7e97
dtmiranda <hortadito2@gmail.com>
2023-03-25 22:24:34 -0100

    feat: implemente student repository

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Repository;
 
 
 /*
-* the repository */
+* the repository anotation used to represent (class/interface) who is responsible for data connection
+* this interface extend JpaRepository that allow us to use methods like(find, findAll, delete.......)
+* the generics represent:
+*       Student is the type of data we work with
+*       Long is the type of the student id.
+*
+* */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +11,14 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudent(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Samuel",
-                        "samuel@email.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        23
-
-
-                )
-        );
+        return studentRepository.findAll();
     }
 }

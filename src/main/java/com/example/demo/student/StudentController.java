8bca99d438b24941130653e81107616cb674b24a
dtmiranda <hortadito2@gmail.com>
2023-11-15 09:37:55 -0100

    Create delete endpoint

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,4 +1,4 @@
-package com.study.springboot.student;
+package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;

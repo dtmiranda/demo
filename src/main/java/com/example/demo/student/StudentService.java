0716a5dd15045a5948c68eebfc523107797b6f7b
dtmiranda <hortadito2@gmail.com>
2023-11-16 11:25:34 -0100

    implement put endpoint

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,11 +40,31 @@ public class StudentService {
         boolean studentExists = studentRepository.existsById(studentId);
 
         if(!studentExists){
-            throw new IllegalStateException("Student with id " + studentId+ "does not exists");
+            throw new IllegalStateException("Student with id " + studentId+ " does not exists");
 
         }
 
         studentRepository.deleteById(studentId);
 
+    }
+
+    @Transactional // This anotatio alow us to up date data without using db query
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()-> new IllegalStateException("Student with id " + studentId + " does not exist"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("Email taken");
+            }
+
+            student.setEmail(email);
+        }
     }
 }
